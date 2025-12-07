2f274ebd16c1b0c6e4ae97e12f984d5a87601406
Alimpos <alimpostea@mail.ru>
2025-12-07 21:23:25 +0300

    refactor: next.config.ts(pathURL-Removed the property)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-    basePath: '/Portfolio',
     pageExtensions: ['tsx', 'ts'],
     env: {
         TELEGRAMM_API:
